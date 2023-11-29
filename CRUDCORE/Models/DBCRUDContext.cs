e9199b5b01a63272199bc30541ca262316ed30ce
karol-macas <klmacas@espe.edu.ec>
2023-11-28 23:17:23 -0500

    Desarrollo de Aplicacion Web con ASP.NET Core

---
+++ CRUDCORE/Models/DBCRUDContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CRUDCORE.Models
+{
+    public partial class DBCRUDContext : DbContext
+    {
+        public DBCRUDContext()
+        {
+        }
+
+        public DBCRUDContext(DbContextOptions<DBCRUDContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cargo> Cargos { get; set; } = null!;
+        public virtual DbSet<Empleado> Empleados { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cargo>(entity =>
+            {
+                entity.HasKey(e => e.IdCargo)
+                    .HasName("PK__CARGO__6C9856258BBBF2E1");
+
+                entity.ToTable("CARGO");
+
+                entity.Property(e => e.Descripcion)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Empleado>(entity =>
+            {
+                entity.HasKey(e => e.IdEmpleado)
+                    .HasName("PK__EMPLEADO__CE6D8B9ECA355833");
+
+                entity.ToTable("EMPLEADO");
+
+                entity.Property(e => e.Correo)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NombreCompleto)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Telefono)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.oCargo)
+                    .WithMany(p => p.Empleados)
+                    .HasForeignKey(d => d.IdCargo)
+                    .HasConstraintName("FK_Cargo");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ README.txt
@@ -0,0 +1,23 @@
+Desarrollo de Aplicación Web con ASP .NET Core
+
+-Descripcion-
+Aplicación Web de ASP.NET Core MVC
+Lenguaje de programación C♯
+Base de datos en SQL Server
+Scaffold-DbContext
+EntityFramework para crear vistas y procesos CRUD automáticos
+
+-Instalacion-
+Para ejecutar este proyecto debes crear la base de datos o importar la base de datos con el nombre BDCRUD y cambiar la cadena de conexión del archivo appsettings.json
+Por ejemplo:
+"ConnectionStrings": {
+  "cadenaSQL": "Server=MSI\\SQLEXPRESS1 ;DataBase=DBCRUD;Integrated Security=true"
+}, 
+
+-Resultados-
+Una vez hecho todo el proceso pones Ejecutar o correr y te abrira el navegador
+y te mostrare la Pagina Web
+
+
+
+
